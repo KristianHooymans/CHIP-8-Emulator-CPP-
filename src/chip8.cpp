34f1174a5e960845863fb2dce0f7a6b7455cf814
KristianHooymans <hooymanskristian@gmail.com>
2026-06-19 21:42:15 +1000

    finished opcodes and adding some comments

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -36,6 +36,8 @@ Chip8::Chip8()
   for (int i = 0; i < FONTSET_SIZE; ++i) {
     memory[FONTSET_START + i] = fontset[i];
   }
+
+
 }
 void Chip8::LoadROM(char const* filename) {
     std::ifstream file(filename, std::ios::binary | std::ios::ate);
@@ -55,28 +57,29 @@ void Chip8::LoadROM(char const* filename) {
         delete[] buffer;
     }
 }
-
+//clear display
 void Chip8::OP_00E0(){
     std::memset(video, 0, sizeof(video));
 }
+//return from subroutine
 void Chip8::OP_00EE() {
     --sp;
     pc = stack[sp];
 }
-
+//jump to address, sets pc to address
 void Chip8::OP_1nnn() {
     uint16_t address = opcode & 0x0FFFu;
 
     pc = address;
 }
-
+//subroutine and address
 void Chip8::OP_2nnn(){
    uint16_t address = opcode & 0x0FFFu;
    stack[sp] = pc;
    ++sp;
    pc = address;
 }
-
+//skip next if Vx = kk
 void Chip8::OP_3xkk() {
     uint8_t Vx = (opcode & 0x0F00u) >> 8u;
     uint8_t byte = opcode & 0x00FFu;
@@ -84,7 +87,7 @@ void Chip8::OP_3xkk() {
         pc += 2;
     }
 }
-
+//Skip next if Vx != kk
 void Chip8::OP_4xkk() {
    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
    uint8_t byte = opcode & 0x00FFu;
@@ -92,7 +95,7 @@ void Chip8::OP_4xkk() {
        pc += 2;
    } 
 }
-
+// skip next instruction if Vx and Vy are equal
 void Chip8::OP_5xy0() {
     uint8_t Vx = (opcode & 0x0F00u) >> 8u;
     uint8_t Vy = (opcode & 0x00F0u) >> 4u;
@@ -101,7 +104,7 @@ void Chip8::OP_5xy0() {
     pc += 2;
     }
 }
-
+// Vx = kk
 void Chip8::OP_6xkk() {
     uint8_t Vx = (opcode & 0x0F00u) >> 8u;
     uint8_t byte = (opcode & 0x00FFu);
@@ -109,6 +112,8 @@ void Chip8::OP_6xkk() {
     registers[Vx] = byte;
 }
 
+
+//Vx = Vx + kk
 void Chip8::OP_7xkk() {
     uint8_t Vx = (opcode & 0x0F00u) >> 8u;
     uint8_t byte = (opcode & 0x00FFu);
@@ -117,7 +122,7 @@ void Chip8::OP_7xkk() {
 
 }
 
-
+//Vx = Vy
 void Chip8::OP_8xy0() {
     uint8_t Vx = (opcode & 0x0F00u) >> 8u;
     uint8_t Vy = (opcode & 0x00F0u) >> 4u;
@@ -125,7 +130,255 @@ void Chip8::OP_8xy0() {
     registers[Vx] = registers[Vy];
 }
 
+//Vx = Vx or Vy
 void Chip8::OP_8xy1() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    
+    registers[Vx] |= registers[Vy];
+}
 
+// vx &= vy
+void Chip8::OP_8xy2() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] &= registers[Vy];
+}
+
+// vx ^= vy
+void Chip8::OP_8xy3() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] ^= registers[Vy];
+}
+
+// vx += vy, vf = 1 if it overflows past 255
+void Chip8::OP_8xy4() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    uint16_t sum = registers[Vx] + registers[Vy];
+
+    if (sum > 255u) {
+        registers[0xF] = 1;
+    } else {
+        registers[0xF] = 0;
+    }
+
+    registers[Vx] = sum & 0xFFu;
+}
+
+// vx -= vy, vf = 1 when there's no borrow (vx > vy)
+void Chip8::OP_8xy5() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    if (registers[Vx] > registers[Vy]) {
+        registers[0xF] = 1;
+    } else {
+        registers[0xF] = 0;
+    }
+
+    registers[Vx] -= registers[Vy];
+}
+
+// shift vx right by 1, vf catches the bit that falls off
+void Chip8::OP_8xy6() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    registers[0xF] = (registers[Vx] & 0x1u);
+    registers[Vx] >>= 1;
+}
+
+// vx = vy - vx, vf = 1 when there's no borrow (vy > vx)
+void Chip8::OP_8xy7() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    if (registers[Vy] > registers[Vx]) {
+        registers[0xF] = 1;
+    } else {
+        registers[0xF] = 0;
+    }
+
+    registers[Vx] = registers[Vy] - registers[Vx];
+}
+
+// shift vx left by 1, vf catches the top bit that falls off
+void Chip8::OP_8xyE() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    registers[0xF] = (registers[Vx] & 0x80u) >> 7u;
+    registers[Vx] <<= 1;
+}
+
+// skip next instr if vx != vy
+void Chip8::OP_9xy0() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    if (registers[Vx] != registers[Vy]) {
+        pc += 2;
+    }
+}
+
+// index = nnn
+void Chip8::OP_Annn() {
+    uint16_t address = opcode & 0x0FFFu;
+
+    index = address;
+}
+
+// jump to nnn + v0
+void Chip8::OP_Bnnn() {
+    uint16_t address = opcode & 0x0FFFu;
+
+    pc = registers[0] + address;
+}
+
+// vx = random byte & kk
+void Chip8::OP_Cxkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    registers[Vx] = randByte(randGen) & byte;
+}
+
+// draw sprite at (vx, vy), n rows tall. vf = 1 if anything gets erased (collision)
+void Chip8::OP_Dxyn() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    uint8_t height = opcode & 0x000Fu;
+
+    uint8_t xPos = registers[Vx] % VIDEO_WIDTH;
+    uint8_t yPos = registers[Vy] % VIDEO_HEIGHT;
+
+    registers[0xF] = 0;
+
+    for (unsigned int row = 0; row < height; ++row) {
+        uint8_t spriteByte = memory[index + row];
+
+        for (unsigned int col = 0; col < 8; ++col) {
+            uint8_t spritePixel = spriteByte & (0x80u >> col);
+            uint32_t* screenPixel = &video[(yPos + row) * VIDEO_WIDTH + (xPos + col)];
+
+            if (spritePixel) {
+                if (*screenPixel == 0xFFFFFFFF) {
+                    registers[0xF] = 1;
+                }
+
+                *screenPixel ^= 0xFFFFFFFF;
+            }
+        }
+    }
+}
+
+// skip next instr if the key stored in vx is being pressed
+void Chip8::OP_Ex9E() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t key = registers[Vx];
+
+    if (keypad[key]) {
+        pc += 2;
+    }
+}
+
+// skip next instr if that key is NOT being pressed
+void Chip8::OP_ExA1() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t key = registers[Vx];
+
+    if (!keypad[key]) {
+        pc += 2;
+    }
+}
+
+// vx = delay timer
+void Chip8::OP_Fx07() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    registers[Vx] = delayTimer;
+}
+
+// freeze here until a key is pressed, then stash it in vx
+void Chip8::OP_Fx0A() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    bool pressed = false;
+
+    for (uint8_t i = 0; i < 16; ++i) {
+        if (keypad[i]) {
+            registers[Vx] = i;
+            pressed = true;
+            break;
+        }
+    }
+
+    if (!pressed) {
+        pc -= 2;
+    }
+}
+
+// delay timer = vx
+void Chip8::OP_Fx15() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    delayTimer = registers[Vx];
+}
+
+// sound timer = vx
+void Chip8::OP_Fx18() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    soundTimer = registers[Vx];
+}
+
+// index += vx
+void Chip8::OP_Fx1E() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    index += registers[Vx];
+}
+
+// point index at the font sprite for the digit in vx (5 bytes each)
+void Chip8::OP_Fx29() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t digit = registers[Vx];
+
+    index = FONTSET_START + (5 * digit);
+}
+
+// split vx into its 3 decimal digits and drop them at index / index+1 / index+2
+void Chip8::OP_Fx33() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t value = registers[Vx];
+
+    memory[index + 2] = value % 10;
+    value /= 10;
+
+    memory[index + 1] = value % 10;
+    value /= 10;
+
+    memory[index] = value % 10;
+}
+
+// dump v0..vx into memory starting at index
+void Chip8::OP_Fx55() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    for (uint8_t i = 0; i <= Vx; ++i) {
+        memory[index + i] = registers[i];
+    }
+}
+
+// read v0..vx back out of memory starting at index
+void Chip8::OP_Fx65() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    for (uint8_t i = 0; i <= Vx; ++i) {
+        registers[i] = memory[index + i];
+    }
 }
 
